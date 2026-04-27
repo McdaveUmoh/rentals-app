06962a5cca9286791b5f7c28232880682779602c
McdaveUmoh <umohmcdave@gmail.com>
2026-04-27 17:24:17 +0100

    logic for ride distance handled

--- rentals/rentals/doctype/ride_order/ride_order.js
+++ rentals/rentals/doctype/ride_order/ride_order.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2026, mcdave umoh and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ride Order", {
-// 	refresh(frm) {
+frappe.ui.form.on("Ride Order", {
+    onload(frm){
+        console.log("running load...");
+    },
+    setup(frm){
+        console.log("setup....");
+    },
+	refresh(frm) {
+        console.log("Refresh")
 
-// 	},
-// });
+        if(frm.doc.status !== "Accepted"){
+            frm.add_custom_button( "Accept", () => {
+            // status = Accepted
+            frm.set_value("status", "Accepted");
+            // save the form
+            frm.save();
+        }
+
+        )
+        }
+        f
+	},
+});
